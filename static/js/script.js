81cab1b7b82e6ef5ee172d9ea3a1507f588a5553
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2024-05-19 03:13:12 +0300

    “adjust_login_button”

--- static/js/script.js
+++ static/js/script.js
@@ -1,18 +1,44 @@
 $(document).ready(function () {
-    $('#signUpForm').on('submit', function (event) {
-        const password = $('#password').val();
-        const confirmPassword = $('#confirmPassword').val();
+    // $('#signUpForm').on('submit', function (event) {
+    //     const phoneNumber = $('#phone');
+    //     if (typeof phoneNumber !== Number) {
+    //         event.preventDefault();
+    //         $('#phoneNumber').addClass('error');
+    //         $('#phoneNumber').after('<span class="error-message">Phone must be number.</span>');
 
-        if (password !== confirmPassword) {
-            event.preventDefault();
-            $('#confirmPassword').addClass('error');
-            $('#confirmPassword').after('<span class="error-message">Passwords do not match. Please try again.</span>');
-        }
-    });
+    //     }
 
-    // Remove error styles and messages when the password fields are changed
-    $('#password, #confirmPassword').on('input', function () {
-        $('#confirmPassword').removeClass('error');
-        $('.error-message').remove();
-    });
+
+    const password = $('#password').val();
+    const confirmPassword = $('#confirmPassword').val();
+
+    if (password !== confirmPassword) {
+        event.preventDefault();
+        $('#confirmPassword').addClass('error');
+        $('#confirmPassword').after('<span class="error-message">Passwords do not match. Please try again.</span>');
+    }
 });
+
+// Remove error styles and messages when the password fields are changed
+$('#password, #confirmPassword').on('input', function () {
+    $('#confirmPassword').removeClass('error');
+    $('.error-message').remove();
+});
+
+// Toggle menu functionality
+$('.toggle-menu').click(function () {
+    $('.links-container').toggleClass('open');
+});
+
+// Optionally close the menu when a link is clicked
+$('.links-container ul li a').click(function () {
+    $('.links-container').removeClass('open');
+});
+
+
+
+
+
+});
+
+
